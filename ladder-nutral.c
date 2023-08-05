4d6104f42997b3f7e598f72c1ae6c3749803c934
Priyanktala <131126830+Priyanktala@users.noreply.github.com>
2023-08-05 10:54:49 -0400

    Add files via upload

---
+++ ladder-nutral.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	
+	printf("Enter value of a=");
+	scanf("%d,&a");
+	
+	
+	if(a<0)
+	{
+		printf("this number is not nutral");
+	}
+	else if(a>0)
+	{
+		printf("This number is not nutral");
+	}
+	else
+	{
+		printf("This number is nutral");
+	}
+}
---
+++ min-if-else.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	
+	printf("Enter value of a=");
+	scanf("%d,&a");
+	
+	printf("Enter value of b=");
+	scanf("%d,&b");
+	
+	if(a<b)
+	{
+		printf("a is minimum",a);
+	}
+	else
+	{
+		printf("b is minimum",b);
+	}
+}
